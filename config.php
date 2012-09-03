1cb2ea57a49005a3d14fd41f48e45c7c25f45c07
Marcel Hecko <maco@blava.net>
2012-09-03 17:13:42 +0200

    fix player images

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 $host		= 'localhost'; 		// host name of database (usually it's localhost)
-$dbUser		= 'somechess';				// username for DB
-$dbPass		= 'parajko';				// password for DB
+$dbUser		= 'somechess';		// username for DB
+$dbPass		= 'parajko';		// password for DB
 $database	= 'somechess';		// DB name (suggested name is somechess, but you can change it of course)
-$dbPre		= '';				// add a prefix to Some Chess table names (optional)
-$lang		= 'en';				// language Some Chess uses
-$firstrun	= false;				// DO NOT EDIT THIS LINE
+$dbPre		= '';			// add a prefix to Some Chess table names (optional)
+$lang		= 'en';			// language Some Chess uses
+$firstrun	= false;		// DO NOT EDIT THIS LINE
+$playerImgDir	= 'playerImg';		//player images directory
 @mysql_connect($host,$dbUser,$dbPass);
 @mysql_select_db($database) or die('ERROR: can\'t connect to database'.mysql_error());
 ?>
